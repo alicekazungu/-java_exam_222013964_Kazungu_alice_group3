6012e4d6b6bf5657f9be79640f2e48b21226b0c8
alicekazungu <151031510+alicekazungu@users.noreply.github.com>
2024-02-19 11:06:41 +0200

    Add files via upload

---
+++ Kazungu Alice,222013964/ally (1).sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2024 at 08:27 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ally`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alice`
+--
+
+CREATE TABLE `alice` (
+  `id` int(23) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `age` int(23) NOT NULL,
+  `gender` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `alice`
+--
+
+INSERT INTO `alice` (`id`, `name`, `age`, `gender`) VALUES
+(1, 'alice', 12, 'female'),
+(2, 'eddy', 23, 'male'),
+(3, 'emmy', 25, 'male'),
+(4, 'ange', 40, 'female'),
+(5, 'alliance', 40, 'female');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alice`
+--
+ALTER TABLE `alice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alice`
+--
+ALTER TABLE `alice`
+  MODIFY `id` int(23) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
